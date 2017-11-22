fb1d33180b1653dfb6679982ad24ba2e969e8515
ElisePetit <petit.elise06@gmail.com>
2017-11-22 17:07:48 +0100

    test unitaire winnerVertical

--- test.pl
+++ test.pl
@@ -1,19 +1,55 @@
 :-["Puissance4.pl"].
 
-%['a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a']
-
 testWinnerHorizontal :- winnerHorizontal(
-['X','X','X','X','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a']
+['X','X','X','X','a','a','a',
+'a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a']
 , 'X', 1),
 winnerHorizontal(
-['a','a','a','a','a','a','a','a','X','X','X','X','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a']
-, 'X', 1),
+['a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a',
+'a','O','O','O','O','a','a']
+, 'O', 1),
  not(winnerHorizontal(
-['X','X','X','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a']
+['X','X','X','a','a','a','a',
+'a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a']
 , 'X', 1)),
  not(winnerHorizontal(
-['a','a','a','a','a','X','X','X','X','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a']
+['a','a','a','a','a','X','X',
+'X','X','a','a','a','a','a',
+'a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a']
 , 'X', 1)).
-%testWinnerVertical :- winnerVertical(Board, P, N).
+
+
+testWinnerVertical :- winnerVertical(
+['a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a',
+'X','a','a','a','a','a','a',
+'X','a','a','a','a','a','a',
+'X','a','a','a','a','a','a',
+'X','a','a','a','a','a','a']
+, 'X', 1),not(winnerVertical(
+['a','a','a','a','a','a','a',
+'a','a','a','a','a','a','a',
+'a','O','a','a','a','a','a',
+'O','O','a','a','a','a','a',
+'O','a','a','a','a','a','a',
+'O','a','a','a','a','a','a']
+, 'O', 1))
+.
+
 %testWinnerDiagonale1 :- winnerDiagonale1(Board, P, N).
 %testWinnerDiagonale2 :- winnerDiagonale2(Board, P, N).
