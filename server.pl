1d0e5a0d820811b7831746abbc2878bfb7c61485
DanhLap <danhlap36@gmail.com>
2017-11-25 17:16:44 +0100

    html marche, mais pas le css

--- server.pl
+++ server.pl
@@ -1,11 +1,18 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_path)).
+:- use_module(library(http/http_authenticate)).
+:- use_module(library(http/http_server_files)).
 
-:- http_handler(/, say_hi, []).		% (1)
 
-server(Port) :-						% (2)
-        http_server(http_dispatch, [port(Port)]).
+:- multifile
+    user:file_search_path/2,
+    http:location/3.
+:- dynamic
+    user:file_search_path/2.
+:- http_handler(css('puissance4.css'),  http_reply_file('pussance4.css', []), []).
 
-say_hi(_Request) :-					% (3)
-        format('Content-type: text/plain~n~n'),
-        format('Hello World!~n').
+:- http_handler('/test',
+		http_reply_file('puissance4.html',[]),[]).
+
+server(Port) :-	http_server(http_dispatch, [port(Port)]).
