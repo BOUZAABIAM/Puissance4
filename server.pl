a6266aa09194162706cd7d5c226f9a7d4aceff8c
DanhLap <danhlap36@gmail.com>
2017-11-27 22:32:23 +0100

    css
    up load fichier css et image sur port 8887 par Web Server For Chrome

--- server.pl
+++ server.pl
@@ -10,7 +10,7 @@
     http:location/3.
 :- dynamic
     user:file_search_path/2.
-:- http_handler(css('puissance4.css'),  http_reply_file('pussance4.css', []), []).
+
 
 :- http_handler('/test',
 		http_reply_file('puissance4.html',[]),[]).
