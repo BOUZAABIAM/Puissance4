fa9d5cf296b65389c29a4b44e985feba264ea29f
DanhLap <danhlap36@gmail.com>
2017-12-04 10:36:15 +0100

    Update server.pl

--- server.pl
+++ server.pl
@@ -1,15 +1,6 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
-:- use_module(library(http/http_path)).
-:- use_module(library(http/http_authenticate)).
-:- use_module(library(http/http_server_files)).
 
-
-:- multifile
-    user:file_search_path/2,
-    http:location/3.
-:- dynamic
-    user:file_search_path/2.
 
 
 :- http_handler('/test',
